ad1f420d7e54f9a4df6c4a59b0a894b6adbd7e47
Shreyash <shreyashdeulkar8@gmail.com>
2026-02-01 23:54:36 +0530

    created a new branch for learning more react concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
-import LudoBoard from './LudoBoard.jsx'
-import TodoList from './TodoList.jsx'
-import Lottery from './Lottery.jsx'
 
 function App() {
 
-  let winconditions = (ticket) => {
-    return sum(ticket)===15;
-  };
+  
   return (
     <>
-      <>
-        {/* <LudoBoard /> */}
-        {/* <TodoList /> */}
-        {/* <Lottery /> */}
-        <Lottery n={3} wincondition={winconditions} />
-      </>
+      
     </>
   )
 }
--- src/TicketNum.jsx
+++
@@ -1,6 +0,0 @@
-import './TicketNUm.css';
-export default function TicketNum({num}) {
-    return (
-        <span className="ticketnum">{num}</span>
-    );
-}

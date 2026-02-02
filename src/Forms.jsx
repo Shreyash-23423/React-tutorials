e9bd89b907a2de18fd6f367866ca1cdf2c4572ea
Shreyash <shreyashdeulkar8@gmail.com>
2026-02-02 22:11:36 +0530

    learned about how to handle multiple inputs of forms

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -1,23 +1,60 @@
 import { useState } from "react"
 
 export default function Forms() {
-  let [fullName, setFullName] = useState("")
+  let [formData, setformData] = useState({
+    fullName: "",
+    username: "",
+    password: ""
+  });
 
-  let handlenamechange = (e) => {
-    setFullName(e.target.value)
+  let handleinputchange = (e) => {
+    let fieldname = e.target.name;
+    let newvalue = e.target.value;
+
+    setformData((currData) => {
+      return {...currData, [fieldname]: newvalue};
+    });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    setformData({
+      fullName: "",
+      username: "",
+      password: ""
+    });
   }
   return (
-   <form >
-    <label htmlFor="username">Full name</label>
-    <input 
-      type="text" 
-      placeholder="enter your full name" 
-      value={fullName}
-      onChange={handlenamechange}
-      id="username"
-      />
-    {/* <input type="text" placeholder="enter your full name" value={fullName} onChange={(e) => setFullName(e.target.value)} /> */}
-    <button>Submit</button>
+   <form onSubmit={handleSubmit}>
+     <label htmlFor="fullname">Full name</label>
+     <input 
+       type="text" 
+       placeholder="enter your full name" 
+       value={formData.fullName}
+       onChange={handleinputchange}
+       name="fullName"
+       />
+       <br />
+       <br />
+     <label htmlFor="username">Username</label>
+     <input 
+       type="text" 
+       placeholder="enter your username" 
+       value={formData.username}
+       onChange={handleinputchange}
+        name="username"
+       />
+     <label htmlFor="password">Password</label>
+     <input 
+       type="text" 
+       placeholder="enter your password" 
+       value={formData.password}
+       onChange={handleinputchange}
+        name="password"
+       />
+     {/* <input type="text" placeholder="enter your full name" value={fullName} onChange={(e) => setFullName(e.target.value)} /> */}
+     <br />
+     <button>Submit</button>
    </form>
   )
 }

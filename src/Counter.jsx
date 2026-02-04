25570d4cc17a6e9ff109a4cd8385af8dcc2f9201
Shreyash <shreyashdeulkar8@gmail.com>
2026-02-04 21:34:18 +0530

    learned about what is a useeffect how to use it now with dependencies

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 export default function Counter(){
-    let [count,setcount]= useState(0);
+    let [countx,setcountx]= useState(0);
+    let [county,setcounty]= useState(0);
 
-    let inccount=()=>{
-        setcount((currcount)=> currcount+1);
+    let inccountx=()=>{
+        setcountx((currcount)=> currcount+1);
+    };
+    let inccounty=()=>{
+        setcounty((currcount)=> currcount+1);
     };
     useEffect(function printsomething(){
         console.log("this is a side-effect")
-    })
+    },[countx]);
     return (
         <div>
-            <h3>count ={count}</h3>
-            <button onClick={inccount}>+1</button>
+            <h3>countx ={countx}</h3>
+            <button onClick={inccountx}>+1</button>
+            <h3>county ={county}</h3>
+            <button onClick={inccounty}>+1</button>
         </div>
     );
     }
